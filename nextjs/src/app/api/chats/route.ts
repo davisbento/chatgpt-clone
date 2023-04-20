8e42f0be5d71d825ab8907d26636405f90f16f4b
davisbento <contato@davisbento.dev>
2023-04-19 21:53:41 -0300

    feat: post and get for chats

--- nextjs/src/app/api/chats/route.ts
+++ nextjs/src/app/api/chats/route.ts
@@ -1,9 +1,16 @@
+import { prisma } from '@/app/prisma/prisma';
 import { NextResponse } from 'next/server';
 
 export async function POST(req: Request) {
-	const body = await req.json();
-
-	return NextResponse.json({
-		body
+	const chatCreated = await prisma.chat.create({
+		data: {}
 	});
+
+	return NextResponse.json({ id: chatCreated.id });
+}
+
+export async function GET(req: Request) {
+	const chats = await prisma.chat.findMany();
+
+	return NextResponse.json(chats);
 }
